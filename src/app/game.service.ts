b026c84d7e5303cd0929297826f4ef0f8f471f83
Webster <strategywebstermanyanga@gmail.com>
2024-02-24 23:35:52 +0100

    Create gameboard
    Created a functional 'play' page with the gameboard and functional tic tac toe logic

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -9,15 +9,56 @@ export class GameService {
     [0, 0, 0],
     [0, 0, 0],
   ]; //This is the board where 1 is 'x', -1 is 'o' and 0 means nothing has been entered
-  currentPlayer = 0;
+  currentPlayer = 1;
 
   setPlayer(player: 1 | -1) {
     this.currentPlayer = player;
   }
 
+  getGame() {
+    return this.gameBoard;
+  }
+
   updateBoard(x: number, y: number) {
     if (this.gameBoard[x][y] !== 0) return;
     this.gameBoard[x][y] = this.currentPlayer;
+
+    this.gameBoard.forEach((row) => {
+      if (row[0] && row[0] === row[1] && row[1] === row[2]) {
+        //win
+        console.log('win1');
+      }
+    });
+
+    for (let i = 0; i < 3; i++) {
+      if (
+        this.gameBoard[0][i] &&
+        this.gameBoard[0][i] === this.gameBoard[1][i] &&
+        this.gameBoard[1][i] === this.gameBoard[2][i]
+      ) {
+        //win
+        console.log('win2');
+      }
+    }
+
+    if (
+      this.gameBoard[1][1] &&
+      this.gameBoard[1][1] === this.gameBoard[0][0] &&
+      this.gameBoard[1][1] === this.gameBoard[2][2]
+    ) {
+      //win
+      console.log('win3');
+    }
+
+    if (
+      this.gameBoard[1][1] && 
+      this.gameBoard[1][1] === this.gameBoard[0][2] &&
+      this.gameBoard[1][1] === this.gameBoard[2][0]
+    ) {
+      //win
+      console.log('win4');
+    }
+
     this.currentPlayer = this.currentPlayer * -1;
   }
 }
--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-play',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './play.component.html',
-  styleUrl: './play.component.scss'
+  styleUrl: './play.component.scss',
 })
 export class PlayComponent {
+  constructor(private gameService: GameService) {}
 
+  gameBoard = this.gameService.getGame();
+
+  played(x: number, y: number) {
+    this.gameService.updateBoard(x, y);
+  }
 }
